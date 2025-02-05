3210850ade686908a90ac129791691478978fbcf
mohammed-wasim-ideassion <mohammed.wasim@ideassion.com>
2025-02-05 10:44:02 +0530

    Added Icons some file changes

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  // plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'transform-react-remove-prop-types',
+      {
+        removeImport: true,
+        additionalLibraries: ['react-style-proptype'],
+      },
+    ],
+  ],
 };
